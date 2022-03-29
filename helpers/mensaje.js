0b635e93797c9ad13e69f40dbc1670ab16384e53
camiloJosue222 <camilojosue222@gmail.com>
2022-03-29 10:55:34 -0500

    test1

---
+++ helpers/mensaje.js
@@ -0,0 +1,56 @@
+const { resolve } = require('path');
+
+require('colors');
+
+const mostrarMenu = () => {
+
+    return new Promise( resolve => {
+
+        console.clear();
+        console.log('==================================='.green);
+        console.log('       SELECCION UNA OPCION        '.green);
+        console.log('===================================\n'.green);
+
+        console.log(`${'1.'.green} crear tarea`);
+        console.log(`${'2.'.green} listar tareas`);
+        console.log(`${'3.'.green} listar tareas completadas`);
+        console.log(`${'4.'.green} listar tareas pendientes`);
+        console.log(`${'5.'.green} completar tarea(s)`);
+        console.log(`${'6.'.green} borrar tarea`);
+        console.log(`${'0.'.green} salir \n`);
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question('Seleccione una opcion: ',(opt) => {
+            readline.close();
+            resolve(opt);
+        })
+
+    });
+
+
+}
+
+const pausa = () => {
+    
+    return new Promise( resolve => {
+        const readline = require('readline').createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+
+            readline.question(`\nPresione ${ 'ENTER'.green } para continuar\n`,(opt) => {
+                readline.close();
+                resolve();
+            })
+    });
+    
+}
+
+module.exports = {
+    mostrarMenu,
+    pausa
+}
